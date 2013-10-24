67473b0a948a2e6021e309d3ad313e1bd7266910
Kevin Varley <kevin@kvarley.co.uk>
2013-10-24 17:28:39 +0100

    Minor commit:  Disabled footer for now.

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
-<div  style="margin:0;padding:0;position:fixed;bottom:0;width:100%;">
+<!--<div  style="margin:0;padding:0;position:fixed;bottom:0;width:100%;">
 	<div id="footer">
 		<div class="left">
 			<a href="?view=state" class="colorbox" style="color:#036;"><?=$status?></a> <span>-</span> 
@@ -11,7 +11,7 @@
 			<?= makePopupLink( '?view=bandwidth', 'zmBandwidth', 'bandwidth', $bwArray[$_COOKIE['zmBandwidth']], ($user && $user['MaxBandwidth'] != 'low' ) ) ?> <?= $SLANG['Bandwidth'] ?>
 		</div>
 	</div>
-</div>
+</div>-->
 
 </body>
 </html>
