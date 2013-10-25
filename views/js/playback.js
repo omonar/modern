5164a26fcdd029a28f5088fa3e2b1bb8a3dc2474
Kevin Varley <kevin@kvarley.co.uk>
2013-10-25 16:42:12 +0100

    Minor commit: Moving noty defaults to the top of the file.

--- views/js/playback.js
+++ views/js/playback.js
@@ -43,6 +43,32 @@ var options = {
   'showCustomTime': true,
   'style': 'box'
 };
+jQuery.noty.defaults = {
+  layout: 'topRight',
+  theme: 'defaultTheme',
+  type: 'alert',
+  text: '',
+  dismissQueue: true, // If you want to use queue feature set this true
+  template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+  animation: {
+      open: {height: 'toggle'},
+      close: {height: 'toggle'},
+      easing: 'swing',
+      speed: 500 // opening & closing animation speed
+  },
+  timeout: 2000, // delay for closing event. Set false for sticky notifications
+  force: false, // adds notification to the beginning of queue when set to true
+  modal: false,
+  maxVisible: 5, // you can set max visible notification for dismissQueue true option
+  closeWith: ['click'], // ['click', 'button', 'hover']
+  callback: {
+      onShow: function() {},
+      afterShow: function() {},
+      onClose: function() {},
+      afterClose: function() {}
+  },
+  buttons: false // an array of buttons
+};
 
 function eventInstance(monitorId, eventId, currentFrame) {
   this.monitorId=monitorId;
@@ -314,32 +340,6 @@ function drawVisualization() {
 }
 
 jQuery(document).ready(function() {
-  jQuery.noty.defaults = {
-    layout: 'topRight',
-    theme: 'defaultTheme',
-    type: 'alert',
-    text: '',
-    dismissQueue: true, // If you want to use queue feature set this true
-    template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
-    animation: {
-        open: {height: 'toggle'},
-        close: {height: 'toggle'},
-        easing: 'swing',
-        speed: 500 // opening & closing animation speed
-    },
-    timeout: 2000, // delay for closing event. Set false for sticky notifications
-    force: false, // adds notification to the beginning of queue when set to true
-    modal: false,
-    maxVisible: 5, // you can set max visible notification for dismissQueue true option
-    closeWith: ['click'], // ['click', 'button', 'hover']
-    callback: {
-        onShow: function() {},
-        afterShow: function() {},
-        onClose: function() {},
-        afterClose: function() {}
-    },
-    buttons: false // an array of buttons
-  };
 
   $("#choose-cameras").dialog({
     autoOpen: false,
