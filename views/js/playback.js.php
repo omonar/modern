097ada9897e5fc8f457a060fac1e90d87fbbccd2
Kevin Varley <kevin@kvarley.co.uk>
2013-10-31 16:40:45 +0000

    Now returns WebColour

--- views/js/playback.js.php
+++ views/js/playback.js.php
@@ -1,4 +1,4 @@
-cameras = JSON.parse('<?= json_encode(dbFetchAll("SELECT Monitors.Id, Monitors.Name, Monitors.Protocol, Monitors.Host, Monitors.Port, Monitors.Path, COUNT(Events.Id) AS Events FROM Monitors INNER JOIN Events ON Events.MonitorId = Monitors.Id GROUP BY Monitors.Id")); ?>
+cameras = JSON.parse('<?= json_encode(dbFetchAll("SELECT Monitors.Id, Monitors.Name, Monitors.Protocol, Monitors.Host, Monitors.Port, Monitors.Path, COUNT(Events.Id) AS Events, Monitors.Width, Monitors.Height FROM Monitors INNER JOIN Events ON Events.MonitorId = Monitors.Id GROUP BY Monitors.Id")); ?>
 ');
 activity = JSON.parse('<?= json_encode(dbFetchAll("SELECT Events.Id, Events.MonitorId, Monitors.Name, Events.StartTime, Events.EndTime AS Date, Events.StartTime, Events.EndTime, Events.Frames, Events.MaxScore FROM Events, Monitors WHERE Events.MonitorId=Monitors.Id AND DATE(EndTime) = DATE(NOW()) ORDER BY EndTime")); ?>
 ');
