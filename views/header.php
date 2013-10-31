87c5d6a728f6fc8fe12d4dfbab33fb4164da8595
Kevin Varley <kevin@kvarley.co.uk>
2013-10-31 11:12:40 +0000

    Minor changes

--- views/header.php
+++ views/header.php
@@ -26,18 +26,20 @@ for ( $i = 0; $i < count($monitors); $i++ )
     $displayMonitors[] = $monitors[$i];
 }
 
-$_view = $_REQUEST['view'];
+if(isset($_REQUEST['view'])) {
+    $_view = $_REQUEST['view'];
+}
 if(empty($_view)) $_view = 'console';
 
 ?>
 <!-- inline styling stops mootools taking over - yes, inline-styles are bad -->
-<div class="btn-group" style="display: inline-block !important;">
-  <button type="button" class="btn btn-default dropdown-toggle mainnav-btn btn-fixedpos" data-toggle="dropdown">
-    Menu <span class="caret"></span>
+<!--<div class="btn-group" style="display: inline-block !important;">
+  <button type="button" class="btn dropdown-toggle mainnav-btn btn-fixedpos ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" data-toggle="dropdown">
+    Menu <span class="ui-button-text"></span>
   </button>
   <ul class="dropdown-menu" role="menu">
     <li><a href="?view=console">Live View</a></li>
     <li><a href="?view=playback">Playback</a></li>
     <li><a href="?view=admin">Admin</a></li>
   </ul>
-</div>
+</div>-->
