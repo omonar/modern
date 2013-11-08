03e21bbe8a217af3bbefb65efb0e9c156c989488
Kevin Varley <kevin@kvarley.co.uk>
2013-11-08 13:40:21 +0000

    Feature added: Fullscreen camera via when you click on a camera stream

--- views/js/playback.js
+++ views/js/playback.js
@@ -639,6 +639,28 @@ jQuery(document).ready(function() { /* begin document ready */
     }
   });
 
+  jQuery(document).on("click", ".monitor-stream-image", function() {
+    var width = ($(window).width()-50);
+    var height = ($(window).height()-50);
+    var monitorWidth = $(this).width();
+    var monitorHeight = $(this).height();
+    var dialogContent = "<div class=\"monitor-stream-dialog\"><img class=\"monitor-stream-fullscreen\" src=\"" + $(this).attr("src") + "\"></div>";
+    $(dialogContent).dialog({
+      modal: true,
+      height: height,
+      width: width,
+      resizable: false,
+      draggable: false
+    });
+    $(".ui-dialog-titlebar-close").html("<span class=\"glyphicon glyphicon-remove\"></span>");
+    if(monitorWidth > monitorHeight) {
+      $(".monitor-stream-fullscreen").css("height", "100%");
+    }
+    else {
+      $(".monitor-stream-fullscreen").css("width", "100%");
+    }
+  });
+
   jQuery(document).on("click", ".show-all-cameras", function(event) {
     event.preventDefault();
     window.stop();
