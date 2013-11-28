71d5882f9b2fe6d6994f2fdad6e14a9a1c6bd817
Kevin Varley <kevin@kvarley.co.uk>
2013-11-28 10:29:38 +0000

    Minor feature release: More admin graphs added, icons added to admin menu.

--- views/admin.php
+++ views/admin.php
@@ -1,16 +1,48 @@
 <?php
   xhtmlHeaders( __FILE__, "Admin" );
-
 ?>
 <body class="zm"> <!-- begin body -->
 
-  <?php include('includes/adminnavbar.php'); ?>
+  <?php
+    include('includes/adminnavbar.php');
+
+    $query = "SELECT Id, Name AS 'MonitorName', WebColour FROM Monitors";
+    $response = dbFetchAll($query);
+    if(!$response) {
+      die("ERROR: Failed to fetch monitor data from database!");
+    }
+    foreach($response as $row) {
+      $monitors[$row['Id']]['MonitorName'] = $row['MonitorName'];
+      $monitors[$row['Id']]['WebColour'] = $row['WebColour'];
+    }
+  ?>
 
   <div class="container"> <!-- begin container -->
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-4">
         <h4>Disk Usage</h4>
-        <canvas id="disk-usage-chart" width="200" height="200"></canvas>
+        <canvas id="disk-usage-chart" width="300" height="300"></canvas>
+      </div>
+
+      <div class="col-md-4">
+        <h4>Event Overview For The Past 7 Days</h4>
+        <canvas id="past-week-chart" width="300" height="300"></canvas>
+      </div>
+
+      <div class="col-md-4">
+        <h4>Most Active Cameras Overview</h4>
+        <canvas id="most-active-cameras-chart" width="300" height="300"></canvas>
+      </div>
+
+      <div class="col-md-2">
+        <h4>Legend</h4>
+        <?php
+          foreach($monitors as $MonitorId => $row) {
+            echo "<p><span class=\"glyphicon glyphicon-stop\" style=\"color: " . $row['WebColour'] . ";\"></span> " . $monitors[$MonitorId]['MonitorName'] . "</p>";
+          }
+        ?>
+          <p><span class="glyphicon glyphicon-stop" style="color: #47A447;"></span> Available Disk Space</p>
+          <p><span class="glyphicon glyphicon-stop" style="color: #D2322D;"></span> Used Disk Space</p>
       </div>
     </div>
   </div> <!-- end container -->
@@ -19,14 +51,92 @@
     var data = [
       {
         value: <?=getDiskPercent()?>,
-        color:"#990033"
+        color:"#D2322D"
       },
       {
         value : <?=(100-getDiskPercent())?>,
-        color : "#29AB87"
+        color : "#47A447"
       }
     ];
     var ctx = document.getElementById("disk-usage-chart").getContext("2d");
+    new Chart(ctx).Pie(data);
+
+    <?php
+      //$query = "SELECT Date(StartTime) AS Date, MonitorId, COUNT(Id) AS NumberOfEvents FROM Events WHERE DATE(StartTime)>=DATE(NOW() - INTERVAL 7 DAY) GROUP BY MonitorId, DAY(StartTime)";
+      $query = "SELECT Date(StartTime) AS Date, Events.MonitorId, Monitors.WebColour, COUNT(Events.Id) AS NumberOfEvents FROM Events, Monitors WHERE Events.MonitorId=Monitors.Id AND DATE(StartTime)>=DATE(NOW() - INTERVAL 7 DAY) GROUP BY MonitorId, DAY(StartTime)";
+      $response = dbFetchAll($query);
+      if(!$response) {
+        die("ERROR: Failed to fetch graph data!");
+      }
+      foreach($response as $row) {
+        $graphData[$row['Date']][$row['MonitorId']]['NumberOfEvents'] = $row['NumberOfEvents'];
+        $graphData[$row['Date']][$row['MonitorId']]['WebColour'] = $row['WebColour'];
+      }
+    ?>
+
+    <?php
+      foreach($monitors as $monitorId => $WebColour) {
+        echo "var dataformonitor{$monitorId} = [";
+        $dataString = "";
+        foreach($graphData as $index => $value) {
+          if(array_key_exists($monitorId, $value)) {
+            $dataString .= $value[$monitorId]['NumberOfEvents'] . ", ";
+          }
+        }
+        echo substr($dataString, 0, -2) . "];\n";
+      }
+    ?>
+
+    <?php
+      echo "var data = {\n";
+      $i = 0;
+      echo "labels : [";
+      foreach($graphData as $index => $value) {
+        $i++;
+        $datetime = strtotime($index);
+        echo "\"" . date("d/m/y", $datetime) . "\"";
+        if($i < sizeof($graphData)) {
+          echo ", ";
+        }
+        else {
+          echo "],";
+        }
+      }
+      echo "\n";
+    ?>
+
+      datasets : [
+
+
+        <?
+          foreach($monitors as $index => $row) {
+            echo "{ fillColor: \"" . $row['WebColour'] . "\", strokeColor: \"" . $row['WebColour'] . "\", pointColor: \"" . $row['WebColour'] . "\", pointStrokeColor: \"#FFF\", data: dataformonitor{$index} },\n";
+          }
+        ?>
+      ]
+    }
+
+    var ctx = document.getElementById("past-week-chart").getContext("2d");
+    new Chart(ctx).Line(data);
+
+    var data = [
+      <?php
+        $query = "SELECT MonitorId, COUNT(Events.Id) AS NumberOfEvents FROM Events GROUP BY MonitorId";
+        $response = dbFetchAll($query);
+        if(!$response) {
+          die("ERROR: Failed to fetch data for chart!");
+        }
+        $i = 0;
+        foreach($response as $row) {
+          $i++;
+          echo "{ value: " . $row['NumberOfEvents'] . ", color: \"" . $monitors[$row['MonitorId']]['WebColour'] . "\" }";
+          if($i < sizeof($response)) {
+            echo ",";
+          }
+        }
+      ?>
+    ];
+    var ctx = document.getElementById("most-active-cameras-chart").getContext("2d");
     new Chart(ctx).Pie(data);
   </script>
 
--- views/includes/adminnavbar.php
+++ views/includes/adminnavbar.php
@@ -11,10 +11,10 @@
     <ul class="nav navbar-nav">
       <li class="colour purple"><a href="?view=playback"><i class="glyphicon glyphicon-chevron-left"></i> Playback</a></li>
 
-      <li class="colour blue"><a href="?view=admin"><span class="glyphicon glyphicon-home"></span></a></li>
+      <li class="colour blue"><a href="?view=admin"><span class="glyphicon glyphicon-home"></span> Home</a></li>
 
       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cameras <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-camera"></span> Cameras <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="?view=monitor" class="init-colorbox"><span class="glyphicon glyphicon-plus-sign"></span> Add camera</a></li>
           <li class="divider"></li>
@@ -37,7 +37,7 @@
     <ul class="nav navbar-nav navbar-right">
 
       <li class="dropdown colour yellow">
-        <a href="#" id="updates" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-tasks"></a>
+        <a href="#" id="updates" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-circle-arrow-up"></span> Updates</a>
         <ul class="dropdown-menu">
           <li id="skinVersionMessage"><a href="#"><span class="glyphicon glyphicon-picture"></span> Checking for updates...</a></li>
         </ul>
@@ -64,7 +64,7 @@
       <li class="colour <?=$colour?>"><a href="#" id="zm-change-state-opener"><span class="glyphicon <?=$icon?>"></span> ZoneMinder is <?=lcfirst($status)?></a></li>
 
       <li class="dropdown colour blue">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=ucfirst($_SESSION['user']['Username'])?> <span class="caret"></span></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?=ucfirst($_SESSION['user']['Username'])?> <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a id="changepassword-opener" href="#"><span class="glyphicon glyphicon-edit"></span> Change Password</a></li>
           <li><a href="?view=logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
